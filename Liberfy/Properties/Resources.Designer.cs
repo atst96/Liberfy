bd715da1f08926722a8add6e707bc7b79582fc3c
atst1996 <tnkjpei04@gmail.com>
2018-02-24 20:03:57 +0900

    PlaceHolderTextコントロールの削除

--- Liberfy/Properties/Resources.Designer.cs
+++ Liberfy/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Liberfy.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   型 System.Byte[] のローカライズされたリソースを検索します。
-        /// </summary>
-        internal static byte[] FontAwesome {
-            get {
-                object obj = ResourceManager.GetObject("FontAwesome", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
