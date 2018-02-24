db93b2011190c59adb3385b9ee6586e2e7f329ba
atst1996 <tnkjpei04@gmail.com>
2018-02-24 20:59:23 +0900

    ツイート表示部分の実装

---
+++ NowPlaying/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowPlaying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
