95b29112b24c55b6138e586834be86d5dc4ac6cb
atst1996 <tnkjpei04@gmail.com>
2018-05-22 01:35:51 +0900

    [SocialApis.Twitter] アップロード時の進捗通知の実装

---
+++ SocialApis/UploadProgressInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialApis
+{
+    public class UploadProgressInfo
+    {
+        public int UploadedSize { get; internal set; }
+        public int TotalSize { get; internal set; }
+        public double UploadPercentage { get; internal set; }
+    }
+}
