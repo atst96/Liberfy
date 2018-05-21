c75eb082309818b60f71755583d7199308b4dcb6
atst1996 <tnkjpei04@gmail.com>
2018-05-22 01:33:57 +0900

    [SocialApis] UploadAPI用MimeTypeの追加

---
+++ SocialApis/MimeTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialApis
+{
+    public static class MimeTypes
+    {
+        public const string OctetStream = "application/octet-stream";
+
+        public static class Video
+        {
+            public const string Mp4 = "video/mp4";
+        }
+    }
+}
