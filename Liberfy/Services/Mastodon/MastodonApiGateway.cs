63c393aa745e41d8cbc6a6e16549c128ed6a357c
atst1996 <tnkjpei04@gmail.com>
2019-02-25 15:46:12 +0900

    [Liberfy.Mastodon] 投稿関連の実装

--- Liberfy/Services/Mastodon/MastodonApiGateway.cs
+++ Liberfy/Services/Mastodon/MastodonApiGateway.cs
@@ -1,4 +1,6 @@
 ﻿using Liberfy.Services.Common;
+using Liberfy.ViewModel;
+using SocialApis;
 using SocialApis.Mastodon;
 using System;
 using System.Collections.Generic;
@@ -8,18 +10,67 @@ using System.Threading.Tasks;
 
 namespace Liberfy.Services.Mastodon
 {
-    internal class MastodonApiGateway : IApiGateway
-    {
-        private readonly MastodonApi _api;
+	internal class MastodonApiGateway : IApiGateway
+	{
+		private readonly MastodonApi _api;
 
-        public MastodonApiGateway(MastodonApi api)
-        {
-            this._api = api;
-        }
+		public MastodonApiGateway(MastodonApi api)
+		{
+			this._api = api;
+		}
 
-        public Task PostStatus(ServicePostParameters parameters)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		private async Task<long> UploadAttachment(UploadMedia attachment)
+		{
+			var media = await this._api.Media.Upload(attachment.SourceStream, attachment.Description, progress: attachment);
+
+			return media.Id;
+		}
+
+		private async Task<long[]> UploadAttachments(ICollection<UploadMedia> attachments)
+		{
+			var tasks = new List<Task<long>>(attachments.Count);
+
+			foreach (var item in attachments)
+			{
+				tasks.Add(this.UploadAttachment(item));
+			}
+
+			return await Task.WhenAll(tasks).ConfigureAwait(false);
+		}
+
+		public async Task PostStatus(ServicePostParameters parameters)
+		{
+			var query = new Query
+			{
+				["status"] = parameters.Text,
+			};
+
+			//if (parameters.ReplyToStatus != null)
+			//{
+			//    query["in_reply_to_id"] = parameters.ReplyToStatus.Id;
+			//}
+
+			if (parameters.Attachments.HasItems)
+			{
+				query["media_ids"] = new QueryArrayItem(await this.UploadAttachments(parameters.Attachments).ConfigureAwait(false));
+			}
+
+			if (parameters.IsContainsWarningAttachment)
+			{
+				query["sensitive"] = true;
+			}
+
+			if (!string.IsNullOrEmpty(parameters.SpoilerText))
+			{
+				query["spoiler_text"] = parameters.SpoilerText;
+			}
+
+			//if (parameters.Visibility != null)
+			//{
+			//    query["visibility"] = GetVisibilityValue(parameters.Visibility);
+			//}
+
+			await this._api.Statuses.Post(query).ConfigureAwait(false);
+		}
+	}
 }
