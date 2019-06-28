29caf32a1d611c61ccea8cdc15fa173368c04248
atst <tnkjpei04@gmail.com>
2019-06-29 00:37:38 +0900

    [update] Config.cs

---
+++ Liberfy/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liberfy
+{
+    internal static partial class Config
+    {
+        public static partial class Twitter
+        {
+            //public const string @ConsumerKey = "";
+            //public const string @ConsumerSecret = "";
+        }
+
+        public static partial class Mastodon
+        {
+        }
+    }
+}
