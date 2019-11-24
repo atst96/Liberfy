22aa3ade483d23f9babed64282e8981601d3ae59
atst_96 <tnkjpei04@gmail.com>
2019-11-24 21:17:14 +0900

    認証ウィンドウのウィザードUIをカスタムコントロール化

---
+++ Liberfy/Controls/WizardPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace Liberfy
+{
+    internal class WizardPage : TabItem
+    {
+    }
+}
