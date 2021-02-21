d4f13d8e088923c02b4a161e91c76e00396e936d
atst1996 <tnkjpei04@gmail.com>
2021-02-21 17:58:37 +0900

    change: アカウント設定情報の項目キーを項目名に変更

--- Liberfy/Data/Settings/Accounts/AccountSettings.cs
+++ Liberfy/Data/Settings/Accounts/AccountSettings.cs
@@ -4,13 +4,16 @@ using System.Collections.Generic;
 
 namespace Liberfy.Settings
 {
+    /// <summary>
+    /// アカウント設定情報
+    /// </summary>
     [MessagePackObject]
     internal class AccountSettings
     {
-        [Key(0)]
+        [Key("accounts")]
         public IReadOnlyList<IAccountSetting> Accounts { get; init; }
 
-        [Key(1)]
+        [Key("columns")]
         public IReadOnlyList<IColumnSetting> Columns { get; init; }
     }
 }
