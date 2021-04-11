98248a80dc9c9abba40eaaa58824784291d00c61
atst1996 <tnkjpei04@gmail.com>
2021-04-11 22:53:46 +0900

    add: コントロール追加

---
+++ Liberfy/Controls/PageSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Liberfy
+{
+    /// <summary>
+    /// ページ切り替えコントロール
+    /// </summary>
+    public class PageSwitcher : TabControl
+    {
+        static PageSwitcher()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PageSwitcher), new FrameworkPropertyMetadata(typeof(PageSwitcher)));
+        }
+
+        /// <summary>
+        /// <paramref name="item"/>がコンテナ内のものかどうかを取得する。
+        /// </summary>
+        /// <param name="item">項目</param>
+        /// <returns></returns>
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is PageSwitcherItem;
+        }
+
+        /// <summary>
+        /// 新しい項目を取得する。
+        /// </summary>
+        /// <returns></returns>
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new PageSwitcherItem();
+        }
+    }
+}
---
+++ Liberfy/Controls/PageSwitcherItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Liberfy
+{
+    /// <summary>
+    /// ページ切り替えコントロールのページ項目
+    /// </summary>
+    public class PageSwitcherItem : TabItem
+    {
+        static PageSwitcherItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(PageSwitcherItem),
+                new FrameworkPropertyMetadata(typeof(PageSwitcherItem)));
+        }
+    }
+}
