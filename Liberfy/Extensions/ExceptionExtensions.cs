143ac3f6428d4d2735afe0754bb38c7975de89d0
atst_96 <tnkjpei04@gmail.com>
2021-09-06 00:02:15 +0900

    add: 例外の拡張メソッドを追加

---
+++ Liberfy/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Liberfy
+{
+    /// <summary>
+    /// 例外に関する拡張メソッド群
+    /// </summary>
+    internal static class ExceptionExtensions
+    {
+        /// <summary>
+        /// 例外の内容をデバッグコンソールに出力する
+        /// </summary>
+        /// <param name="e"></param>
+        public static void DumpDebug(this Exception e)
+        {
+            Debug.WriteLine("== 例外スタックトレース開始 ==");
+            Debug.WriteLine(e.Message);
+            Debug.WriteLine(e.StackTrace);
+            Debug.WriteLine("== 例外スタックトレース終了 ==");
+        }
+    }
+}
