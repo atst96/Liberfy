0ac1a55c830a9fcd4fc8567bf35bc19ca0e1b347
atst_96 <tnkjpei04@gmail.com>
2021-09-05 23:55:19 +0900

    add: ダイアログ関連のAction/Messageを追加

---
+++ Liberfy/Behaviors/Messaging/DialogInteractionMessageAction.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Liberfy.Messaging;
+using Livet.Behaviors.Messaging;
+using Livet.Messaging;
+
+namespace Liberfy.Behaviors.Messaging
+{
+    internal class DialogInteractionMessageAction : InteractionMessageAction<FrameworkElement>
+    {
+        /// <summary>
+        /// ウィンドウハンドル
+        /// </summary>
+        private IntPtr _hwnd;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (this.AssociatedObject.IsLoaded)
+            {
+                this.Initialize();
+            }
+            else
+            {
+                this.AssociatedObject.Loaded += this.OnLoaded;
+            }
+        }
+
+        /// <summary>
+        /// Loadedイベント発行時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.AssociatedObject.Loaded -= this.OnLoaded;
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        private void Initialize()
+        {
+            var window = Window.GetWindow(this.AssociatedObject);
+            this._hwnd = new WindowInteropHelper(window).Handle;
+        }
+
+        /// <summary>
+        /// アクション実行
+        /// </summary>
+        /// <param name="message"></param>
+        protected override void InvokeAction(InteractionMessage message)
+        {
+            switch (message)
+            {
+                case InformationMessage infoMessage:
+                    this.InformationMessage(infoMessage);
+                    return;
+
+                case InformationDialogMessage infoDialogMessage:
+                    this.InformationDialogMessage(infoDialogMessage);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// ダイアログ表示
+        /// </summary>
+        /// <param name="message"></param>
+        private void InformationMessage(InformationMessage message)
+        {
+            _ = TaskDialog.ShowDialog(this._hwnd, new()
+            {
+                Caption = message.Caption,
+                Text = message.Text,
+                Icon = GetDialogIcon(message.Image),
+            });
+        }
+
+        /// <summary>
+        /// <see cref="MessageBoxImage"/>から<see cref="TaskDialogIcon"/>を取得する
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        private static TaskDialogIcon GetDialogIcon(MessageBoxImage image) => image switch
+        {
+            MessageBoxImage.None => TaskDialogIcon.None,
+            MessageBoxImage.Error => TaskDialogIcon.Error,
+            MessageBoxImage.Question => TaskDialogIcon.Information,
+            MessageBoxImage.Exclamation => TaskDialogIcon.Warning,
+            MessageBoxImage.Asterisk => TaskDialogIcon.Error,
+            _ => TaskDialogIcon.None,
+        };
+
+        /// <summary>
+        /// ダイアログ表示
+        /// </summary>
+        /// <param name="message"></param>
+        private void InformationDialogMessage(InformationDialogMessage message)
+        {
+            var footnote = message.FootNoteText != null ? null : new TaskDialogFootnote
+            {
+                Text = message.FootNoteText,
+                Icon = message.FootNoteIcon,
+            };
+
+            var expander = message.ExpanderText == null ? null : new TaskDialogExpander
+            {
+                Text = message.ExpanderText,
+                Expanded = message.IsExpanded,
+                Position = message.ExpanderPosition,
+            };
+
+            _ = TaskDialog.ShowDialog(this._hwnd, new()
+            {
+                Caption = message.Caption,
+                Text = message.Text,
+                Icon = message.Icon,
+                Heading = message.Heading,
+                AllowCancel = message.AllowCancel,
+                AllowMinimize = message.AllowMimimize,
+                Expander = expander,
+                Footnote = footnote,
+            });
+        }
+    }
+}
---
+++ Liberfy/Messaging/InformationDialogMessage.cs
@@ -0,0 +1,201 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using Livet.Messaging;
+
+namespace Liberfy.Messaging
+{
+    public class InformationDialogMessage : InteractionMessage
+    {
+        public InformationDialogMessage() : base()
+        {
+        }
+
+        public InformationDialogMessage(string messageKey) : base()
+        {
+            this.MessageKey = messageKey;
+        }
+
+        protected override Freezable CreateInstanceCore() => new InformationDialogMessage(this.MessageKey)
+        {
+            Caption = this.Caption,
+            Text = this.Text,
+            Icon = this.Icon,
+            Heading = this.Heading,
+            ExpanderText = this.ExpanderText,
+            AllowCancel = this.AllowCancel,
+            AllowMimimize = this.AllowMimimize,
+            IsExpanded = this.IsExpanded,
+            ExpanderPosition = this.ExpanderPosition,
+            FootNoteIcon = this.FootNoteIcon,
+            FootNoteText = this.FootNoteText,
+        };
+
+
+        /// <summary>
+        /// ダイアログのタイトルを取得または設定する
+        /// </summary>
+        public string Caption
+        {
+            get => this.GetValue(CaptionProperty) as string;
+            set => this.SetValue(CaptionProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="Caption"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register(nameof(Caption), typeof(string), typeof(InformationDialogMessage), new PropertyMetadata(null));
+
+        /// <summary>
+        /// ダイアログのテキストを取得または設定する
+        /// </summary>
+        public string Text
+        {
+            get => this.GetValue(TextProperty) as string;
+            set => this.SetValue(TextProperty, value);
+        }
+
+        /// <summary>
+        /// ダイアログの本文のタイトルを取得または設定する
+        /// </summary>
+        public string Heading
+        {
+            get { return (string)GetValue(HeadingProperty); }
+            set { SetValue(HeadingProperty, value); }
+        }
+
+        /// <summary>
+        /// <see cref="Heading"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty HeadingProperty =
+            DependencyProperty.Register("Heading", typeof(string), typeof(InformationDialogMessage), new PropertyMetadata(null));
+
+        /// <summary>
+        /// <see cref="Text"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(InformationDialogMessage), new PropertyMetadata(null));
+
+        /// <summary>
+        /// ダイアログのアイコンを取得または設定する
+        /// </summary>
+        public TaskDialogIcon? Icon
+        {
+            get => this.GetValue(IconProperty) as TaskDialogIcon;
+            set => this.SetValue(IconProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="Icon"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(TaskDialogIcon), typeof(InformationDialogMessage), new PropertyMetadata(null));
+
+
+
+        public bool AllowCancel
+        {
+            get => (bool)this.GetValue(AllowCancelProperty);
+            set => this.SetValue(AllowCancelProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="AllowCancel"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty AllowCancelProperty =
+            DependencyProperty.Register(nameof(AllowCancel), typeof(bool), typeof(InformationDialogMessage), new PropertyMetadata(true));
+
+        public bool AllowMimimize
+        {
+            get => (bool)this.GetValue(AllowMinimizeProperty);
+            set => this.SetValue(AllowMinimizeProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="AllowMimimize"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty AllowMinimizeProperty =
+            DependencyProperty.Register(nameof(AllowMimimize), typeof(bool), typeof(InformationDialogMessage), new PropertyMetadata(false));
+
+
+
+        /// <summary>
+        /// 埋め込みテキストを取得または設定する
+        /// </summary>
+        public string ExpanderText
+        {
+            get => this.GetValue(ExpanderTextProperty) as string;
+            set => this.SetValue(ExpanderTextProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="ExpanderText"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty ExpanderTextProperty =
+            DependencyProperty.Register(nameof(ExpanderText), typeof(string), typeof(InformationDialogMessage), new PropertyMetadata(null));
+
+
+
+        /// <summary>
+        /// 埋め込みテキストの初期表示を非表示にするかどうかを取得または設定する
+        /// </summary>
+        public bool IsExpanded
+        {
+            get => (bool)this.GetValue(IsExpandedProperty);
+            set => this.SetValue(IsExpandedProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="IsExpanded"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register(nameof(IsExpanded), typeof(bool), typeof(InformationDialogMessage), new PropertyMetadata(false));
+
+
+        /// <summary>
+        /// 埋め込みテキストの表示位置を取得または設定する
+        /// </summary>
+        public TaskDialogExpanderPosition ExpanderPosition
+        {
+            get => (TaskDialogExpanderPosition)this.GetValue(expanderPositionProperty);
+            set => this.SetValue(expanderPositionProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="ExpanderPosition"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty expanderPositionProperty =
+            DependencyProperty.Register(nameof(ExpanderPosition), typeof(TaskDialogExpanderPosition), typeof(InformationDialogMessage), new PropertyMetadata(default(TaskDialogExpanderPosition)));
+
+
+        /// <summary>
+        /// フッターに表示する内容を取得または設定する
+        /// </summary>
+        public string FootNoteText
+        {
+            get => this.GetValue(FootNoteTextProperty) as string;
+            set => this.SetValue(FootNoteTextProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="FootNoteText"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty FootNoteTextProperty =
+            DependencyProperty.Register(nameof(FootNoteText), typeof(string), typeof(InformationDialogMessage), new PropertyMetadata(null));
+
+        /// <summary>
+        /// フッターのアイコンを取得または設定する
+        /// </summary>
+        public TaskDialogIcon FootNoteIcon
+        {
+            get => this.GetValue(taskDialogIconProperty) as TaskDialogIcon;
+            set => this.SetValue(taskDialogIconProperty, value);
+        }
+
+        /// <summary>
+        /// <see cref="FootNoteIcon"/>のプロパティ
+        /// </summary>
+        public static readonly DependencyProperty taskDialogIconProperty =
+            DependencyProperty.Register(nameof(FootNoteIcon), typeof(TaskDialogIcon), typeof(InformationDialogMessage), new PropertyMetadata(null));
+    }
+}
